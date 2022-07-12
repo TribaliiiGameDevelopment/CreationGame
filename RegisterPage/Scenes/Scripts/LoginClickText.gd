50bc178c3d3e640334b8ac77ebf1e9d444b68490
Trung Lam <107538119+trunglam107@users.noreply.github.com>
2022-07-11 23:05:22 -0700

    added main register scene

---
+++ RegisterPage/Scenes/Scripts/LoginClickText.gd
@@ -0,0 +1,13 @@
+extends Button
+
+#insert login scene path here
+var loginScene = "res://Scenes/testScene.tscn"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed", self, "_login_pressed")
+
+#when login text is pressed on, change to login scene
+func _login_pressed():
+	get_tree().change_scene(loginScene)
+
