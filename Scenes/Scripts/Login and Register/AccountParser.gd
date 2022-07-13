68b2820ac6ee344f019aff6374677001eaa392c6
JihadBeydoun <jihad.beydoun@tribaliii.org>
2022-07-13 11:43:24 -0400

    Updated Accounts.json, Added AccountParser script

---
+++ Scenes/Scripts/Login and Register/AccountParser.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var path = "res://JSON/Accounts/Accounts.json"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
