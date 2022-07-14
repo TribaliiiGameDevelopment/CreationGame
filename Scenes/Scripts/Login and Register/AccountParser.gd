10d6eca34b2140b2fc428c162ca1cc196951fcd3
JihadBeydoun <jihad.beydoun@tribaliii.org>
2022-07-14 14:58:43 -0400

    Added comments
    Added comments describing what each line of code in my scripts does.

--- Scenes/Scripts/Login and Register/AccountParser.gd
+++ Scenes/Scripts/Login and Register/AccountParser.gd
@@ -1,6 +1,6 @@
 extends Node
 
-#Finds the Accounts.json file -Jihad
+#Finds the Accounts.json file and stores it into variable called json_path  -Jihad
 var json_path = "res://JSON/Accounts/Accounts.json"
 
 #Account data array -Jihad
@@ -8,18 +8,19 @@ var account_data = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#Creates object of class File called jsonFile. This will be used to store the Accounts.json file. -Jihad
 	var jsonFile = File.new()
 	
+	#We open and read Accounts.json and store it into jsonFile. -Jihad
 	jsonFile.open(json_path, jsonFile.READ)
 	
-	#Stores the data within Accounts.json as a string in the accounts_text variable -Jihad
+	#Parses and stores the data within Accounts.json as a string in the accounts_text variable -Jihad
 	var accounts_text = JSON.parse(jsonFile.get_as_text())
 	
 	#Parses the Accounts.json through the accounts_text variable and stores it in account_data. -Jihad
 	account_data = accounts_text.result
 	
-	#Json parsing is completed so we "close" it so that the program will no longer read the Accounts.json file. -Jihad
+	#Json parsing is completed so we "close" it so that the program will no longer read the Accounts.json file. This is to save memory usage. -Jihad
 	jsonFile.close()
 	
-	print(account_data)
 #-----------FINISHED PARSING THE JSON -Jihad----------------------
--- Scenes/Scripts/Login and Register/LoginPress.gd
+++ Scenes/Scripts/Login and Register/LoginPress.gd
@@ -12,14 +12,19 @@ func _on_button_pressed():
 	username = get_node("../Username Field").get_text()
 	password = get_node("../Password Field").get_text()
 	
-	if (AccountParser.account_data.has(username)):
+	#Login info check -Jihad
+	if (AccountParser.account_data.has(username)): #Checks if the entered username is present in Accounts.json -Jihad
+		#If it's present, the debug log will output "Found" and will continue onto the password check. -Jihad
 		print("Found")
-		if (password != str(AccountParser.account_data[username].Password)):
+		if (password != str(AccountParser.account_data[username].Password)): #Checks if the enetered password does not match the account password in Accounts.json -Jihad
+			#If it does not match, the WrongPassword node will become visible notifying the user that they entered the wrong password, and "Wrong Password" will be printed to the debug log -JIhad
+			get_node("../WrongPassword").popup_centered()
 			print("Wrong Password")
 		else:
+			#If the password is correct, the user will log into the game -Jihad
 			print("Logged in!")
 	else:
+		#If a username not present in Accounts.json is entered, the AccountNotFound node will become visible notifying the user that the account was not found and "Not Found" will be printed tot he debug log -Jihad
 		get_node("../AccountNotFound").popup_centered()
 		print("Not Found")
 		
-	print(password)
